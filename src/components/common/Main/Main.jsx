e98a4dd82535c3c260d4e3c8de33278e7c9ea002
NataliaJeszke <nataliajeszke8@gmail.com>
2023-10-22 17:45:22 +0200

    style for main

--- src/components/common/Main/Main.jsx
+++ src/components/common/Main/Main.jsx
@@ -1,8 +1,8 @@
 import InputWindow from "../../SpeechRecognition/InputWindow";
-
+import styles from "./Main.module.css";
 export default function Main() {
   return (
-    <main>
+    <main className={styles.main_container}>
       <InputWindow />
     </main>
   );
